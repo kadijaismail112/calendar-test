3a62ce0e143c4aa7707472bff6939c82b7ed3b75
Kadija Ismail <kadijaismail112@gmail.com>
2023-07-26 22:34:46 -0400

    going back

--- trial.py
+++ trial.py
@@ -5,10 +5,6 @@ from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
-from pydantic import BaseModel, Field
-from typing import Optional
-
-from parser import create_json
 
 # If modifying these scopes, delete the file token.json.
 SCOPES = ['https://www.googleapis.com/auth/calendar']
@@ -41,25 +37,6 @@ SCOPES = ['https://www.googleapis.com/auth/calendar']
 #   },
 # }
 
-class Event(BaseModel):
-    summary: str = Field(description="title of the event", example='Meeting with John')
-    location: Optional[str] = Field(description="where the event is, or the location of the event", 
-                                    example="800 Howard St., San Francisco, CA 94103")
-    description: Optional[str] = Field(description="What the event is about", 
-                                        example="Discussing the new project")
-    start_time: str = Field(description="The start time of the event, in datetime format", 
-                            example="2021-10-10T09:00:00-07:00")
-    end_time: str = Field(description="The end time of the event, in datetime format, if not specified set to an hour later than start time", 
-                            example="2021-10-10T10:00:00-07:00")
-    recurrence: Optional[str] = Field(description="How often is the meeting happening", 
-                                        example="RRULE:FREQ=DAILY;COUNT=2")
-    email1: Optional[str] = Field(description="The email of another person sharing the invite", 
-                                    example="kadija@")
-    email2: str = Field (description="Another email that is being used to share with this person", 
-                        example="kadija@")
-    reminders: dict = Field(description="How often you want to be reminded of the event", 
-                            example="{'useDefault': False, 'overrides': [{'method': 'email', 'minutes': 24 * 60}, {'method': 'popup', 'minutes': 10}]}")
-
 def create_event(event_json):
     """Shows basic usage of the Google Calendar API.
     Prints the start and name of the next 10 events on the user's calendar.
@@ -91,7 +68,3 @@ def create_event(event_json):
     except HttpError as error:
         print('An error occurred: %s' % error)
         return 0 
-
-if __name__ == '__main__':
-    event_j = create_json("Create a calendar event for tomorrow at 3pm", Event)
-    create_event(event_j)
